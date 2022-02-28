ff6496367d5b78490dc2cddc4a4110beb2401c10
Sean <sean.tan3@ucalgary.ca>
2022-02-28 16:30:31 -0700

    sean first commit

--- src/App.java
+++ src/App.java
@@ -15,6 +15,7 @@
  * 		    30064340
  * 
  * @author	Sean Honour Yuan An Tan
+ * 			30094560
  * 			
  * 
  * 
@@ -80,9 +81,28 @@ public class App {
 			//need a text message here
 			return;
 		}
+		//for goalies, add to team and add relevant goalie statistics
+		if (position == "goalie"){
+			teams.get(team_name).add(player_name);
+			ages.put(player_name, age);
+			countries.put(player_name, country);
+			positions.put(player_name, position);
+			jerseys.put(player_name, jersey);
+			saves.put(player_name, "0");
+		}
 		
-		//add player name to arraylist corresponding to team name in hashmap
-		teams.get(team_name).add(player_name);
+		//for non goalie players, add to team and add relevant non goalie statistics
+		if(position != "goalie"){
+			teams.get(team_name).add(player_name);
+			ages.put(player_name, age);
+			countries.put(player_name, country);
+			positions.put(player_name, position);
+			jerseys.put(player_name, jersey);
+			goals.put(player_name, "0");
+			assists.put(player_name, "0");
+			shots_on_goal.put(player_name, "0");
+		}
+
 		
 		/**
 		 * For all other stat hashmaps, add player name as key and add
@@ -90,14 +110,7 @@ public class App {
 		 * 
 		 * Numerical values are defaulted to a String 0
 		 */
-		ages.put(player_name, age);
-		countries.put(player_name, country);
-		positions.put(player_name, position);
-		jerseys.put(player_name, jersey);
-		goals.put(player_name, "0");
-		saves.put(player_name, "0");
-		assists.put(player_name, "0");
-		shots_on_goal.put(player_name, "0");
+
 		
 	}
 
@@ -118,7 +131,32 @@ public class App {
 		goals.replace(player_name, String.valueOf(goals_as_int));
 		
 	}
-	
+	/**
+	 * Increment the saves statistic tied to a goalie
+	 */
+	public static void add_save(String player_name, int num_saves){
+		int saves_as_int = Integer.parseInt(saves.get(player_name));
+		saves_as_int += num_saves;
+		goals.replace(player_name, String.valueOf(saves_as_int));
+	}
+
+	/** Increment the number of shots on goal statistic tied to a player
+	 *
+	 */
+	public static void add_shot_on_goal(String player_name, int shot_on_goal_num){
+		int shot_on_goal_int = Integer.parseInt(shots_on_goal.get(player_name));
+		shot_on_goal_int += shot_on_goal_num;
+		shots_on_goal.replace(player_name, String.valueOf(shot_on_goal_int));
+	}
+
+	/**
+	 * Increments the number of saves statistic tied to a player
+	 */
+	public static void add_assist(String player_name, int assists_num){
+		int assist_int = Integer.parseInt(assists.get(player_name));
+		assist_int += assists_num;
+		assists.replace(player_name, String.valueOf(assist_int));
+	}
 	/**
 	 * Print summary of player statistic
 	 * 
@@ -257,6 +295,28 @@ public class App {
 			player_summary(player);
 		}
 		
+	}
+
+
+	/**
+	 * find and print top 5 assisters in league
+	 * @param args
+	 */
+
+	public static void score_and_save_percent(String player_name) {
+		String position = positions.get(player_name);
+		if (position == "goalie") {
+			System.out.println(player_name + "'s save percentage is:");
+			save_percent =
+
+		}
+
+		if (position != "goalie"){
+			System.out.println(player_name + "'s goal percentage is:");
+			float goal_percent = ((goals.get(player_name)) / (shots_on_goal.get(player_name))) * 100;
+			System.out.println(goal_percent);
+
+		}
 	}
 	
 	
