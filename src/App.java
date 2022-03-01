f097058a137596f0a7dbd787ae71336f7d793981
Sean Tan <seanhonourtan@gmail.com>
2022-02-28 23:53:07 -0700

    JUNIT push

--- src/App.java
+++ src/App.java
@@ -25,9 +25,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Flow.Subscriber;
 import java.util.Collections;
-import java.util.Scanner;
 
 import javax.swing.plaf.synth.SynthSpinnerUI;
 
@@ -40,15 +38,15 @@ import java.util.Arrays;
  *
  */
 public class App {
-	
+
 	/*
 	 * HashMaps for storing dat
-	 * 
+	 *
 	 * Teams are stored with Team Names as Keys, mapped to an Arraylist of player names
 	 */
-	private static HashMap<String, ArrayList<String>> teams;
-	
-	/*All other HashMaps are <String, String>, 
+	public static HashMap<String, ArrayList<String>> teams;
+
+	/*All other HashMaps are <String, String>,
 	 *keys are player names mapped to specific stats for each player
 	 */
 	public static HashMap<String, String> ages;
@@ -64,19 +62,32 @@ public class App {
 
 	/**
 	  * Adds a team to the Teams HashMap
-	  * 
+	  *
 	  * @param team_name
 	  */
 	public static void add_team(String team_name) {
+
+		// if team_name string value is invalid dont add
+		if(team_name == null || team_name.equals("")){
+			System.out.println("ERROR! Team name must be a proper string");
+			return;
+		}
+		// if team already exists dont add
+		if(teams.containsKey(team_name)){
+			System.out.println("Error! This team already exists!");
+			return;
+		}
+
+
 		//Create a key, value pair mapping teamname to empty arraylist for players
 		teams.put(team_name, new ArrayList<String>());
 	}
-	
+
 	/**
 	 * Adds a player to the data
-	 * 
-	 * 
-	 * @param team_name
+	 *
+	 *
+	 * @param team_name ddf
 	 * @param player_name
 	 * @param age
 	 * @param country
@@ -87,6 +98,7 @@ public class App {
 		//if player is in a team already, don't add
 		if (teams.get(team_name).contains(player_name)) {
 			//need a text message here
+			System.out.println("ERROR! Player already exists in this team!");
 			return;
 		}
 		//for goalies, add to team and add relevant goalie statistics
@@ -98,7 +110,7 @@ public class App {
 			jerseys.put(player_name, jersey);
 			saves.put(player_name, "0");
 		}
-		
+
 		//for non goalie players, add to team and add relevant non goalie statistics
 		if(position != "goalie"){
 			teams.get(team_name).add(player_name);
@@ -111,38 +123,43 @@ public class App {
 			shots_on_goal.put(player_name, "0");
 		}
 
-		
+
 		/**
 		 * For all other stat hashmaps, add player name as key and add
 		 * parameters as values
-		 * 
+		 *
 		 * Numerical values are defaulted to a String 0
 		 */
 
-		
+
 	}
 
 	/**
 	 * Increment the goal statistic tied to a player
-	 * 
-	 * 
+	 *
+	 *
 	 * @param player_name
 	 * @param num_goals
 	 */
 	public static void add_goal(String player_name, int num_goals) {
+		if(num_goals < 0){
+			System.out.println("ERROR! Number of goals must be positive!");
+			return;
+		}
 		/*
 		 * parse goal stat as int from hashmap, increment it by parameter, then replace in hashmap
 		 */
 		int goals_as_int = Integer.parseInt(goals.get(player_name));
 		goals_as_int += num_goals;
 		goals.replace(player_name, String.valueOf(goals_as_int));
-		
+
 	}
-	
+
 	/**
 	 * Increment the saves statistic tied to a goalie
 	 */
 	public static void add_save(String player_name, int num_saves){
+
 		int saves_as_int = Integer.parseInt(saves.get(player_name));
 		saves_as_int += num_saves;
 		goals.replace(player_name, String.valueOf(saves_as_int));
@@ -152,6 +169,7 @@ public class App {
 	 *
 	 */
 	public static void add_shot_on_goal(String player_name, int shot_on_goal_num){
+
 		int shot_on_goal_int = Integer.parseInt(shots_on_goal.get(player_name));
 		shot_on_goal_int += shot_on_goal_num;
 		shots_on_goal.replace(player_name, String.valueOf(shot_on_goal_int));
@@ -161,6 +179,7 @@ public class App {
 	 * Increments the number of saves statistic tied to a player
 	 */
 	public static void add_assist(String player_name, int assists_num){
+
 		int assist_int = Integer.parseInt(assists.get(player_name));
 		assist_int += assists_num;
 		assists.replace(player_name, String.valueOf(assist_int));
@@ -174,19 +193,19 @@ public class App {
 		shot_on_goalie_int += shot_on_goalie_num;
 		shots_on_goalie.replace(player_name, String.valueOf(shot_on_goalie_int));
 	}
-	
+
 	/**
 	 * Print summary of player statistic
-	 * 
-	 * 
-	 * 
+	 *
+	 *
+	 *
 	 * @param player_name
 	 */
 	public static void player_summary(String player_name) {
 		//array of category names for formatting
-		String[] categories = {"Age", "Country of Origin", "Position", "#" , "Goals", "Saves", "Assists", "Shots on Goal"}; 
+		String[] categories = {"Age", "Country of Origin", "Position", "#" , "Goals", "Saves", "Assists", "Shots on Goal"};
 		System.out.println(player_name + "\n-----------------------------------\n");
-		
+
 		//print each stat that corresponds to player name  key in respective hashmap
 		for (int i = 0; i < categories.length; ++i) {
 			System.out.println(String.format("       -%s:  %s", categories[i], all_maps.get(i).get(player_name)));
@@ -196,13 +215,13 @@ public class App {
 
 	/**
 	 * Print all players according to a team
-	 * 
+	 *
 	 * @param team_name
 	 * @param full_details - true if looking for summary of each player, false if looking just for name
 	 */
 	public static void print_players_by_team(String team_name, boolean full_details) {
 		System.out.println(team_name + "\n-----------------------------------\n");
-		
+
 		/**
 		 * Loop to print just names of players
 		 */
@@ -225,13 +244,13 @@ public class App {
 
 	/**
 	 * Print all players in the league, sorted into teams
-	 * 
+	 *
 	 * @param full_details - False for just player names, true for player summaries
 	 */
 	public static void print_league(boolean full_details) {
 		System.out.println("Full Leage Review:");
 		System.out.println("-----------------------------------\n");
-		
+
 		//Print just names
 		if (!full_details) {
 			//iterate through each team name in teams hashmap
@@ -272,13 +291,13 @@ public class App {
 			System.out.println(String.format("       -%s", team));
 		}
 		System.out.println();
-	}	
+	}
 
-		
+
 	/**
 	 * Find player in league that has the most goals
-	 * 
-	 * 
+	 *
+	 *
 	 */
 	public static void highest_goals() {
 		System.out.println("Player with the most Goals:\n-----------------------------------\n");
@@ -286,7 +305,7 @@ public class App {
 		ArrayList<String> highest_scorers = new ArrayList<String>();
 		//each player starts with 0 goals, counter from 0
 		int most_goals = 0;
-		
+
 		//iterate through each team name in teams hashmap
 		for (String team : teams.keySet()) {
 			//iterate through each player name in arraylist
@@ -297,7 +316,7 @@ public class App {
 				}
 			}
 		}
-		
+
 		//same for loop as above
 		for (String team : teams.keySet()) {
 			for (String player : teams.get(team)) {
@@ -307,12 +326,12 @@ public class App {
 				}
 			}
 		}
-		
+
 		//print summaries for each player with highest goals
 		for (String player : highest_scorers) {
 			player_summary(player);
 		}
-		
+
 	}
 
 
@@ -334,11 +353,11 @@ public class App {
 					top_counts.add(count);
 				}
 			}
-			
+
 			else {
 				for (int i = 0; i < number_of; ++i) {
 					top_counts.add(all_counts.get(all_counts.size() - 1 - i));
-				}	
+				}
 			}
 
 			for (int count : top_counts) {
@@ -350,90 +369,94 @@ public class App {
 			}
 	}
 
-	public static void top_assisters(int number_of) {
-		System.out.println(String.format("Top %d Assisters:\n-----------------------------------\n", number_of));
-			ArrayList<Integer> all_counts = new ArrayList<Integer>();
-			ArrayList<Integer> top_counts = new ArrayList<Integer>();
+public static void top_assisters(int number_of) {
+	System.out.println(String.format("Top %d Assisters:\n-----------------------------------\n", number_of));
+		ArrayList<Integer> all_counts = new ArrayList<Integer>();
+		ArrayList<Integer> top_counts = new ArrayList<Integer>();
 
 
-			for (Map.Entry<String, String> set : assists.entrySet()) {
-				all_counts.add(Integer.parseInt(set.getValue()));
-			}
+		for (Map.Entry<String, String> set : assists.entrySet()) {
+			all_counts.add(Integer.parseInt(set.getValue()));
+		}
 
-			Collections.sort(all_counts);
-			Collections.reverse(all_counts);
+		Collections.sort(all_counts);
+		Collections.reverse(all_counts);
 
-			if (all_counts.size() < number_of) {
-				for (int count : all_counts) {
-					top_counts.add(count);
-				}
-			}
-			
-			else {
-				for (int i = 0; i < number_of; ++i) {
-					top_counts.add(all_counts.get(all_counts.size() - 1 - i));
-				}	
-			}
-
-			for (int count : top_counts) {
-				for (Map.Entry<String, String> key : assists.entrySet()) {
-					if (Integer.parseInt(assists.get(key.getKey())) == count) {
-						player_summary(key.getKey());
-					}
-				}
+		if (all_counts.size() < number_of) {
+			for (int count : all_counts) {
+				top_counts.add(count);
 			}
 		}
 
-
-	public static void top_goalis(int number_of) {
-		System.out.println(String.format("Top %d Goalies:\n-----------------------------------\n", number_of));
-			ArrayList<Integer> all_counts = new ArrayList<Integer>();
-			ArrayList<Integer> top_counts = new ArrayList<Integer>();
-
-
-			for (Map.Entry<String, String> set : saves.entrySet()) {
-				all_counts.add(Integer.parseInt(set.getValue()));
-			}
-
-			Collections.sort(all_counts);
-			Collections.reverse(all_counts);
-
-			if (all_counts.size() < number_of) {
-				for (int count : all_counts) {
-					top_counts.add(count);
-				}
-			}
-			
-			else {
-				for (int i = 0; i < number_of; ++i) {
-					top_counts.add(all_counts.get(all_counts.size() - 1 - i));
-				}	
-			}
-
-			for (int count : top_counts) {
-				for (Map.Entry<String, String> key : saves.entrySet()) {
-					if (Integer.parseInt(saves.get(key.getKey())) == count) {
-						player_summary(key.getKey());
-					}
-				}
+		else {
+			for (int i = 0; i < number_of; ++i) {
+				top_counts.add(all_counts.get(all_counts.size() - 1 - i));
 			}
 		}
 
+		for (int count : top_counts) {
+			for (Map.Entry<String, String> key : assists.entrySet()) {
+				if (Integer.parseInt(assists.get(key.getKey())) == count) {
+					player_summary(key.getKey());
+				}
+			}
+		}
+	}
 
 
+public static void top_goalis(int number_of) {
+	System.out.println(String.format("Top %d Goalies:\n-----------------------------------\n", number_of));
+		ArrayList<Integer> all_counts = new ArrayList<Integer>();
+		ArrayList<Integer> top_counts = new ArrayList<Integer>();
 
 
+		for (Map.Entry<String, String> set : saves.entrySet()) {
+			all_counts.add(Integer.parseInt(set.getValue()));
+		}
+
+		Collections.sort(all_counts);
+		Collections.reverse(all_counts);
+
+		if (all_counts.size() < number_of) {
+			for (int count : all_counts) {
+				top_counts.add(count);
+			}
+		}
+
+		else {
+			for (int i = 0; i < number_of; ++i) {
+				top_counts.add(all_counts.get(all_counts.size() - 1 - i));
+			}
+		}
+
+		for (int count : top_counts) {
+			for (Map.Entry<String, String> key : saves.entrySet()) {
+				if (Integer.parseInt(saves.get(key.getKey())) == count) {
+					player_summary(key.getKey());
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Calculates and displays individual players score/save percentage
+	 * @param player_name
+	 */
 	public static void score_and_save_percent(String player_name) {
+		//get player position ,store in variable
 		String position = positions.get(player_name);
+		//set player name to lower case to avoid having same player with different capitalization
 		player_name = player_name.toLowerCase();
-		if ("goalie".equals(position)) {
+		// if player position is goalie, calculate total saves divided by
+		if ("goalie".equals(positions.get(player_name))) {
 			System.out.println(player_name + "'s save percentage is:");
 			float save_percent = (Integer.parseInt((saves.get(player_name))) / (Integer.parseInt(shots_on_goalie.get(player_name))));
 			System.out.println(save_percent);
 
 		}
 
-		if (!"goalie".equals(position)){
+		if (!"goalie".equals(positions.get(player_name))){
 			System.out.println(player_name + "'s goal percentage is:");
 			float goal_percent = ((Integer.parseInt(goals.get(player_name))) / (Integer.parseInt(shots_on_goal.get(player_name)))) * 100;
 			System.out.println(goal_percent);
@@ -538,13 +561,13 @@ public class App {
 
 
 
-	/**************************************************************************************
-	 * UI SECTION *************************************************************************
-	 * ************************************************************************************
-	 * ************************************************************************************
-	 * ************************************************************************************
-	 * ************************************************************************************
-	 */
+/**************************************************************************************
+ * UI SECTION *************************************************************************
+ * ************************************************************************************
+ * ************************************************************************************
+ * ************************************************************************************
+ * ************************************************************************************
+ */
 
 
 	public static void input_add_team() {
@@ -1085,9 +1108,47 @@ public class App {
 		all_maps.add(assists);
 		all_maps.add(shots_on_goal);
 
-		
-		
+
+
+		add_team("Man City");
+		add_team("Man United");
+		add_player("Man United", "M", "40", "Argentina", "Striker", "69");
+		add_player("Man United", "Me", "40", "Argentina", "Striker", "69");
+		add_player("Man United", "Mes", "40", "Argentina", "Striker", "69");
+
+		add_player("Man City", "R", "40", "Porugal", "Stiker", "420");
+		add_player("Man City", "Ro", "40", "Porugal", "Stiker", "420");
+		add_player("Man City", "Ron", "40", "Porugal", "Stiker", "420");
+		add_player("Man City", "Rona", "40", "Porugal", "Stiker", "420");
+		add_player("Man City", "Ronal", "40", "Porugal", "Stiker", "420");
+
+
+
+		//print__teams();
+		//print_players_by_team("Man City", true);
+		//player_summary("Ronaldo");
+
+		add_goal("M", 1);
+		add_goal("Rona", 2);
+		add_goal("R", 5);
+		add_goal("Ro", 20);
+		add_goal("Ron", 40);
+		//top_scorers(40);
+		sum_table(goals, "Goals");
+
+
 		run();
+
+
+
+
+
+
+
+
+
+
+		//add_team("Man City");
 
 
 
--- src/AppTest.java
+++ src/AppTest.java
@@ -5,38 +5,97 @@ import java.util.ArrayList;
 class AppTest {
 
     @Test
-    void add_team() {
-        String team_name = "Manchester United";
-       App.add_team(team_name);
+    void add_team_key_test() {
+        String team_name_test = "man united";
+        App.add_team(team_name_test);
+        assertTrue(App.teams.containsKey(team_name_test));
 
-
-
+    }
+    void add_team_is_empty() {
+        String team_name_test = "man united";
+        App.add_team(team_name_test);
+        assertFalse(App.teams.isEmpty());
 
 
     }
 
     @Test
-    void add_player() {
+    void add_player_key_test() {
+        App.add_player("man united", "Ronaldo", "38", "Portugal", "Striker", "9");
+        assertTrue(App.teams.containsValue("Ronaldo"));
+    }
+    @Test
+    void add_player_is_empty() {
+        String player_name_test = "Ronaldo";
+        App.add_player("man united", "Ronaldo", "38", "Portugal", "Striker", "9");
+        assertFalse(App.teams.isEmpty());
     }
 
     @Test
-    void add_goal() {
+    void add_goal_key_test() {
+
+        String player_name_test = "Ronaldo";
+        App.add_goal(player_name_test, 5);
+        assertTrue(App.goals.containsKey(player_name_test));
+    }
+    @Test
+    void add_goal_is_empty() {
+        String player_name_test = "Ronaldo";
+        App.add_goal(player_name_test, 5);
+        assertFalse(App.goals.isEmpty());
     }
 
     @Test
-    void add_save() {
+    void add_save_key_test() {
+        String player_name_test = "Neuer";
+        App.add_save(player_name_test, 5);
+        assertTrue(App.saves.containsKey(player_name_test));
+    }
+    @Test
+    void add_save_is_empty() {
+        String player_name_test = "Neuer";
+        App.add_save(player_name_test, 5);
+        assertFalse(App.saves.isEmpty());
     }
 
     @Test
-    void add_shot_on_goal() {
+    void add_shot_on_goal_key_test() {
+        String player_name_test = "Ronaldo";
+        App.add_shot_on_goal(player_name_test, 5);
+        assertTrue(App.shots_on_goal.containsKey(player_name_test));
+    }
+    @Test
+    void add_shot_on_goal_empty() {
+        String player_name_test = "Ronaldo";
+        App.add_shot_on_goal(player_name_test, 5);
+        assertFalse(App.shots_on_goal.isEmpty());
     }
 
     @Test
-    void add_assist() {
+    void add_assist_empty() {
+        String player_name_test = "Ronaldo";
+        App.add_assist(player_name_test, 5);
+        assertFalse(App.assists.isEmpty());
+
+    }
+    @Test
+    void add_assist_key_test() {
+        String player_name_test = "Ronaldo";
+        App.add_assist(player_name_test, 5);
+        assertTrue(App.assists.containsKey(player_name_test));
+
     }
 
     @Test
-    void add_shot_on_goalie() {
+    void add_shot_on_goalie_empty() {
+        String player_name_test = "Neuer";
+        App.add_shot_on_goalie(player_name_test, 5);
+        assertFalse(App.shots_on_goalie.isEmpty());
+    }@Test
+    void add_shot_on_goalie_key_test() {
+        String player_name_test = "Neuer";
+        App.add_assist(player_name_test, 5);
+        assertTrue(App.assists.containsKey(player_name_test));
     }
 
     @Test
