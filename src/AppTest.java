8b6a757edfb03fe75a183182c495836c9b4cd645
Sean Tan <seanhonourtan@gmail.com>
2022-02-28 20:56:22 -0700

    setup inital junit5 test

---
+++ src/AppTest.java
@@ -0,0 +1,94 @@
+class AppTest {
+
+    @org.junit.jupiter.api.Test
+    void add_team() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_player() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_goal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_save() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_shot_on_goal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_assist() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add_shot_on_goalie() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void player_summary() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void print_players_by_team() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void print_league() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void print__teams() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void highest_goals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void top_scorers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void top_assisters() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void top_goalis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void score_and_save_percent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mean_table() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void median_table() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sum_table() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void min_table() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void max_table() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void run() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
